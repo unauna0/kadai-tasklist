22f981384735b3d4f91060f18642daf0c587fed6
yuuna.kurushima <031420.yuu@gmail.com>
2022-03-06 13:25:56 +0000

    column again

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_065012) do
+ActiveRecord::Schema.define(version: 2022_03_06_122801) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
