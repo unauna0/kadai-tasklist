bbc2eeb8e5d91e28472f29ada2ab4010808fc7ab
yuuna.kurushima <031420.yuu@gmail.com>
2022-03-13 12:47:48 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to: "tasks#index"
   
   resources :tasks
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
   end
